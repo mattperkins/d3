3eb4274e09c2e686d745cd52c088127f9c81ad49
mattperkins <github@mattperkins.com>
2018-10-30 23:14:14 +0000

    update

--- d3-from-data/index.js
+++ d3-from-data/index.js
@@ -1,13 +1,12 @@
 const data = [
- {
-  width: 200,
-  height: 150,
-  fill: '#f5d443'
- }
+ {width: 200, height: 100, fill: '#f5d443'},
+ {width: 100, height: 60, fill: '#354'},
+ {width: 50, height: 30, fill: '#44ff'}
 ]
 
-let svg = d3.select("svg").append("rect")
- 
+const svg = d3.select("svg")
+
+const rect = svg.selectAll("rect") 
  .data(data)
  .attr('width', x => x.width )
  .attr('height', x => x.height )
