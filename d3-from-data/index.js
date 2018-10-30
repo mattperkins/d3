613d42ce298199a1dfe765246c14135838b91d7e
mattperkins <github@mattperkins.com>
2018-10-30 22:10:31 +0000

    update

--- d3-from-data/index.js
+++ d3-from-data/index.js
@@ -1,15 +1,7 @@
-const data = [
- {
-  width: 200,
-  height: 200,
-  fill: 'purple'
- }
-]
 
 const svg = d3.select("svg")
 
 svg.select('rect')
- .data()
  .attr('width', 200)
  .attr('height', 200)
  .attr('fill', 'pink')
