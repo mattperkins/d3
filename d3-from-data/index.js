6caf051e2c4adcaa4178eeef7d8f58992c7ab21c
mattperkins <github@mattperkins.com>
2018-10-30 22:25:52 +0000

    update

--- d3-from-data/index.js
+++ d3-from-data/index.js
@@ -1,7 +1,14 @@
-
-const svg = d3.select("svg")
-
-svg.select('rect')
- .attr('width', 200)
- .attr('height', 200)
- .attr('fill', 'pink')
+const data = [
+ {
+  width: 200,
+  height: 150,
+  fill: 'yellow'
+ }
+]
+const svg = d3.select('svg')
+ 
+const rect = svg.select('rect')
+ .data(data)
+ .attr('width', (x)=>{ return x.width })
+ .attr('height', (x)=>{ return x.height })
+ .attr('fill', (x)=>{ return x.fill })
