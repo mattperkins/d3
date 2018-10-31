5cfecf5449850dc1f0be37118ad03184a5b30420
mattperkins <github@mattperkins.com>
2018-10-31 00:43:29 +0000

    update

---
+++ d3-from-data/fakeData.js
@@ -0,0 +1,23 @@
+const cy = 100
+// select svg container first
+const svg = d3.select("svg")
+
+d3.json('fakeData.json').then(data =>{
+ const circs = svg.selectAll('circle')
+  .data(data)
+
+// add attrs to circs already in DOM (if any exist)
+     .attr('cy', cy)
+     .attr('cx', d => d.distance)
+     .attr('r', d => d.radius)
+     .attr('fill', d => d.fill)
+
+// append the enter selection to DOM (add to existing)
+  circs.enter()
+   .append('circle')
+     .attr('cy', cy) 
+     .attr('cx', d => d.distance)
+     .attr('r', d => d.radius)
+     .attr('fill', d => d.fill)
+ 
+})
