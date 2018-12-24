c6228571b06caaed31c7e2acd30f999cdef28af6
mattperkins <github@mattperkins.com>
2018-12-24 01:02:23 +0000

    cl

--- d3-from-data/fakeData.js
+++ d3-from-data/fakeData.js
@@ -1,33 +1,33 @@
-const cy = 300;
+const cy = 300
 // select svg container first
-const svg = d3.select("svg");
+const svg = d3.select('svg')
 
-toggleCol = (function() {
+const toggleCol = (function () {
   // var currentCol = "white"
 
-  return function() {
+  return function () {
     //  currentCol = currentCol == "white" ? "red" : "white"
-    d3.select(this).style("stroke", "#111");
-  };
-})();
+    d3.select(this).style('stroke', '#111')
+  }
+})()
 
-d3.json("fakeData.json").then(data => {
-  const circs = svg.selectAll("circle").data(data);
+d3.json('fakeData.json').then(data => {
+  const circs = svg.selectAll('circle').data(data)
 
   // add attrs to circs already in DOM (if any exist)
   circs
-    .attr("cy", cy)
-    .attr("cx", d => d.distance)
-    .attr("r", d => d.radius)
-    .attr("fill", d => d.fill);
+    .attr('cy', cy)
+    .attr('cx', d => d.distance)
+    .attr('r', d => d.radius)
+    .attr('fill', d => d.fill)
 
   // append the enter selection to DOM (add to existing)
   circs
     .enter()
-    .append("circle")
-    .attr("cy", cy)
-    .attr("cx", d => d.distance)
-    .attr("r", d => d.radius)
-    .attr("fill", d => d.fill)
-    .on("click", toggleCol);
-});
+    .append('circle')
+    .attr('cy', cy)
+    .attr('cx', d => d.distance)
+    .attr('r', d => d.radius)
+    .attr('fill', d => d.fill)
+    .on('click', toggleCol)
+})
--- d3-from-data/index.js
+++ d3-from-data/index.js
@@ -1,23 +1,23 @@
 const data = [
- {width: 200, height: 100, fill: '#f5d443'},
- {width: 100, height: 60, fill: '#354'},
- {width: 50, height: 30, fill: '#44ff'}
+  { width: 200, height: 100, fill: '#f5d443' },
+  { width: 100, height: 60, fill: '#354' },
+  { width: 50, height: 30, fill: '#44ff' }
 ]
 
-const svg = d3.select("svg")
+const svg = d3.select('svg')
 
 // join data to rects
-const rects = svg.selectAll("rect") 
- .data(data)
+const rects = svg.selectAll('rect')
+  .data(data)
 
 // add attrs to rects already in the DOM
-rects.attr('width', x => x.width )
- .attr('height', x => x.height )
- .attr('fill', x => x.fill )
+rects.attr('width', x => x.width)
+  .attr('height', x => x.height)
+  .attr('fill', x => x.fill)
 
 // append the enter selection to DOM
 rects.enter()
- .append("rect")
- .attr('width', x => x.width )
- .attr('height', x => x.height )
- .attr('fill', x => x.fill )
+  .append('rect')
+  .attr('width', x => x.width)
+  .attr('height', x => x.height)
+  .attr('fill', x => x.fill)
